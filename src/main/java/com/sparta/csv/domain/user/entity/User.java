63b4f4ed8603a691621d05571b12a0566335e3a6
임PRO스터\su992 <su9926@naver.com>
2025-02-01 20:04:24 +0900

    feat: User Entity 추가

--- src/main/java/com/sparta/csv/domain/user/entity/User.java
+++ src/main/java/com/sparta/csv/domain/user/entity/User.java
@@ -1,4 +1,40 @@
 package com.sparta.csv.domain.user.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name="users")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+
+	@Column(nullable = false)
+	private String nickName;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Builder
+	public User(String nickName, String email, String password){
+		this.nickName=nickName;
+		this.email=email;
+		this.password=password;
+	}
 }
