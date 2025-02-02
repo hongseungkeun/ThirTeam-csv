bb51f19359e1ab68d4a4dacbbd4dbeb2d067c63e
임PRO스터\su992 <su9926@naver.com>
2025-02-03 07:38:39 +0900

    Feat: updateUserById 메서드 추가

--- src/main/java/com/sparta/csv/domain/user/service/UserService.java
+++ src/main/java/com/sparta/csv/domain/user/service/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.sparta.csv.domain.user.dto.request.UserInfoRequest;
 import com.sparta.csv.domain.user.dto.response.UserInfoResponse;
 import com.sparta.csv.domain.user.entity.User;
 import com.sparta.csv.domain.user.repository.UserRepository;
@@ -31,4 +32,14 @@ public class UserService {
 
 		userRepository.delete(user);
 	}
+
+	public void updateUserById(Long userId, UserInfoRequest userInfoRequest) {
+		User user = userRepository.findById(userId).orElseThrow(
+			() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "조회되는 회원 정보가 없습니다. id: "+userId)
+		);
+
+		user.updateNickName(userInfoRequest.nickname());
+	}
+
+	/* Todo User 조회 메서드 따로 추출하기 */
 }
