fda60112a88eb2114fa6228b43b56681c2b08287
임PRO스터\su992 <su9926@naver.com>
2025-02-03 00:48:11 +0900

    Feat: Signin 메서드 추가

--- src/main/java/com/sparta/csv/domain/user/service/AuthService.java
+++ src/main/java/com/sparta/csv/domain/user/service/AuthService.java
@@ -1,13 +1,18 @@
 package com.sparta.csv.domain.user.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.sparta.csv.domain.user.dto.request.SigninRequest;
 import com.sparta.csv.domain.user.dto.request.SignupRequest;
+import com.sparta.csv.domain.user.dto.response.SigninResponse;
 import com.sparta.csv.domain.user.entity.User;
 import com.sparta.csv.domain.user.enums.UserRole;
 import com.sparta.csv.domain.user.repository.UserRepository;
 
+import jakarta.security.auth.message.AuthException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -32,5 +37,23 @@ public class AuthService {
 			.build();
 
 		User savedUser = userRepository.save(newUser);
+	}
+
+	public SigninResponse signin(@Valid SigninRequest signinRequest) {
+		User user = userRepository.findByEmail(signinRequest.email()).orElseThrow(
+			() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "가입되지 않은 유저입니다.")
+		);
+
+		if (!BCrypUtil.matches(signinRequest.password(), user.getPassword())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 비밀번호입니다.");
+		}
+
+		String bearerToken = createBearerToken(user);
+
+		return SigninResponse.from(bearerToken);
+	}
+
+	private String createBearerToken(User user) {
+		return jwtUtil.createToken(user.getUserId(), user.getEmail(), user.getNickName(), user.getUserRole());
 	}
 }
