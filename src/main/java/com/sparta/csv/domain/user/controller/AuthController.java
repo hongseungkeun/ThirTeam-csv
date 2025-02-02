48631a592809b4e614328bf733f69b4187a6c769
임PRO스터\su992 <su9926@naver.com>
2025-02-03 00:15:03 +0900

    Feat: AuthController 추가
    - signup API 추가

---
+++ src/main/java/com/sparta/csv/domain/user/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.sparta.csv.domain.user.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sparta.csv.domain.user.dto.request.SignupRequest;
+import com.sparta.csv.domain.user.service.AuthService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+	private final AuthService authService;
+
+	@PostMapping("/signup")
+	public ResponseEntity<Void> signup(@Valid @RequestBody SignupRequest signupRequest){
+		authService.signup(signupRequest);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+
+
+}
