708b744d863651562c4bad43d931ca3338bac3f3
임PRO스터\su992 <su9926@naver.com>
2025-02-03 06:40:06 +0900

    Feat: UserService 추가
    - getUser 메서드 추가

---
+++ src/main/java/com/sparta/csv/domain/user/service/UserService.java
@@ -0,0 +1,26 @@
+package com.sparta.csv.domain.user.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.sparta.csv.domain.user.dto.response.UserInfoResponse;
+import com.sparta.csv.domain.user.entity.User;
+import com.sparta.csv.domain.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+	private final UserRepository userRepository;
+
+	public UserInfoResponse getUser(Long userId) {
+		User user=userRepository.findById(userId).orElseThrow(
+			() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "조회되는 회원 정보가 없습니다. id: "+userId)
+		);
+
+		return UserInfoResponse.from(user);
+	}
+}
