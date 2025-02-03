17b59d722476c596e475bc540ca0d8ecdd45f467
임PRO스터\su992 <su9926@naver.com>
2025-02-03 10:54:29 +0900

    Feat: updateUserPasswordById 메서드 추가

--- src/main/java/com/sparta/csv/domain/user/service/UserService.java
+++ src/main/java/com/sparta/csv/domain/user/service/UserService.java
@@ -1,10 +1,13 @@
 package com.sparta.csv.domain.user.service;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.sparta.csv.domain.user.dto.request.UserInfoRequest;
+import com.sparta.csv.domain.user.dto.request.UserPasswordRequest;
 import com.sparta.csv.domain.user.dto.response.UserInfoResponse;
 import com.sparta.csv.domain.user.entity.User;
 import com.sparta.csv.domain.user.repository.UserRepository;
@@ -39,6 +42,23 @@ public class UserService {
 		);
 
 		user.updateNickName(userInfoRequest.nickname());
+	}
+
+	@Transactional
+	public void updateUserPasswordById(Long userId, UserPasswordRequest userPasswordRequest) {
+		User user = userRepository.findById(userId).orElseThrow(
+			() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "조회되는 회원 정보가 없습니다. id: "+userId)
+		);
+
+		if(!BCrypUtil.matches(userPasswordRequest.password(), user.getPassword())){
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 비밀번호입니다.");
+		}
+
+		if(BCrypUtil.matches(userPasswordRequest.newPassword(), user.getPassword())){
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "기존과 동일한 비밀번호입니다.");
+		}
+
+		user.updatePassword(BCrypUtil.encrypt(userPasswordRequest.newPassword()));
 	}
 
 	/* Todo User 조회 메서드 따로 추출하기 */
