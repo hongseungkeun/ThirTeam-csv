def48da1e92c0510b19392e7ee21e153e2c95bb7
SeuneKeun <eiziong1@gmail.com>
2025-02-03 20:10:40 +0900

    feat: 예매 등록 기능 구현

--- src/main/java/com/sparta/csv/domain/booking/service/BookingService.java
+++ src/main/java/com/sparta/csv/domain/booking/service/BookingService.java
@@ -5,9 +5,11 @@ import com.sparta.csv.domain.booking.entity.BookedSeat;
 import com.sparta.csv.domain.booking.entity.Booking;
 import com.sparta.csv.domain.booking.repository.BookingRepository;
 import com.sparta.csv.domain.screening.entity.Screening;
+import com.sparta.csv.domain.screening.service.ScreeningService;
 import com.sparta.csv.domain.seat.entity.Seat;
 import com.sparta.csv.domain.seat.service.SeatService;
 import com.sparta.csv.domain.user.entity.User;
+import com.sparta.csv.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,15 +21,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BookingService {
 
+    private final UserService userService;
+    private final ScreeningService screeningService;
     private final SeatService seatService;
+    private final BookedSeatService bookedSeatService;
     private final BookingRepository bookingRepository;
 
     public Long registration(Long userId, Long screeningId, BookingCreateRequest request) {
-        // 유저 필요
-        User user = null;
-
-        // 상영 정보 필요
-        Screening screening = null;
+        User user = userService.findUserById(userId);
+        Screening screening = screeningService.findScreeningById(screeningId);
 
         Booking booking = Booking.builder()
                 .screening(screening)
@@ -38,7 +40,7 @@ public class BookingService {
                 .map(seatId -> {
                     Seat seat = seatService.findSeatById(seatId);
 
-                    // 좌석 예약 중복 처리 로직 필요
+                    bookedSeatService.existsByBookingAndSeat(booking, seat);
 
                     return BookedSeat.builder()
                             .booking(booking)
