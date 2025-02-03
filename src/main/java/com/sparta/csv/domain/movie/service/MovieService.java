9303782170b7d679c26cd0cbe9e9d2675f4459e0
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-02-03 10:37:03 +0900

    Feat: 영화검색 구현

--- src/main/java/com/sparta/csv/domain/movie/service/MovieService.java
+++ src/main/java/com/sparta/csv/domain/movie/service/MovieService.java
@@ -5,6 +5,8 @@ import com.sparta.csv.domain.movie.dto.response.MovieResponse;
 import com.sparta.csv.domain.movie.entity.Movie;
 import com.sparta.csv.domain.movie.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import static com.sparta.csv.domain.movie.entity.Movie.newMovie;
@@ -13,9 +15,15 @@ import static com.sparta.csv.domain.movie.entity.Movie.newMovie;
 @RequiredArgsConstructor
 public class MovieService {
     private final MovieRepository movieRepository;
-    public MovieResponse createMovie(CreateMovieRequest req){
+
+    public MovieResponse createMovie(CreateMovieRequest req) {
         Movie movie = newMovie(req);
         movie = movieRepository.save(movie);
         return MovieResponse.from(movie);
+    }
+
+    public Page<MovieResponse> findAllMovies(String title, Pageable pageable) {
+        Page<Movie> movies = movieRepository.findAllByTitle(title, pageable);
+        return movies.map(MovieResponse::from);
     }
 }
