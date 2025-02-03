7f7328bbb0715fb629cd8efe98856b3493e306f6
Lustella-123 <tella4164@gmail.com>
2025-02-03 13:24:52 +0900

    Refactor: ScreeningResponse static from 매서드 추가

--- src/main/java/com/sparta/csv/domain/screening/dto/response/ScreeningResponse.java
+++ src/main/java/com/sparta/csv/domain/screening/dto/response/ScreeningResponse.java
@@ -1,8 +1,18 @@
 package com.sparta.csv.domain.screening.dto.response;
 
+import com.sparta.csv.domain.screening.entity.Screening;
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
+@Builder
 public record ScreeningResponse(
         Long theaterId,
         LocalDateTime screeningTime) {
+    public static ScreeningResponse from(Screening screening) {
+        return ScreeningResponse.builder()
+                .theaterId(screening.getScreeningId())
+                .screeningTime(screening.getShowTime())
+                .build();
+    }
 }
