a6936eb9eb39bf1a578d19cc87bdefbbd897f6c9
Lustella-123 <tella4164@gmail.com>
2025-02-03 11:44:56 +0900

    Refactor: ScreeningResponse DTO 클래스명 변경

--- src/main/java/com/sparta/csv/domain/screening/dto/response/ScreeningResponse.java
+++ src/main/java/com/sparta/csv/domain/screening/dto/response/ScreeningResponse.java
@@ -2,7 +2,7 @@ package com.sparta.csv.domain.screening.dto.response;
 
 import java.time.LocalDateTime;
 
-public record screeningResponse(
+public record ScreeningResponse(
         Long theaterId,
         LocalDateTime screeningTime) {
 }
