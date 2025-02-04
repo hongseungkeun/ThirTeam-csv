d9df7b3895b72631d7c1a3750cefb9bda129f876
임PRO스터\su992 <su9926@naver.com>
2025-02-04 10:20:40 +0900

    Refactor: 커스텀 예외처리 적용

--- src/main/java/com/sparta/csv/domain/user/service/UserService.java
+++ src/main/java/com/sparta/csv/domain/user/service/UserService.java
@@ -1,13 +1,10 @@
 package com.sparta.csv.domain.user.service;
 
 import com.sparta.csv.global.config.BCrypUtil;
-import com.sparta.csv.global.exception.ForbiddenException;
-import com.sparta.csv.global.exception.error.ErrorCode;
+import com.sparta.csv.global.exception.*;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.sparta.csv.domain.user.dto.request.SigninRequest;
 import com.sparta.csv.domain.user.dto.request.UserInfoRequest;
@@ -15,6 +12,7 @@ import com.sparta.csv.domain.user.dto.request.UserPasswordRequest;
 import com.sparta.csv.domain.user.dto.response.UserInfoResponse;
 import com.sparta.csv.domain.user.entity.User;
 import com.sparta.csv.domain.user.repository.UserRepository;
+import com.sparta.csv.global.exception.error.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -47,16 +45,16 @@ public class UserService {
 
 	@Transactional
 	public void updateUserPasswordById(Long userId, UserPasswordRequest userPasswordRequest) {
-		User user = userRepository.findById(userId).orElseThrow(
-			() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "조회되는 회원 정보가 없습니다. id: " + userId)
-		);
+		User user = findUserById(userId);
 
+		// 입력한 비밀번호 불일치
 		if (!BCrypUtil.matches(userPasswordRequest.password(), user.getPassword())) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 비밀번호입니다.");
+			throw new BadRequestException(ErrorCode.INVALID_PASSWORD);
 		}
 
+		// 기존과 동일한 비밀번호로 수정 불가
 		if (BCrypUtil.matches(userPasswordRequest.newPassword(), user.getPassword())) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "기존과 동일한 비밀번호입니다.");
+			throw new BadRequestException(ErrorCode.INVALID_PASSWORD);
 		}
 
 		user.updatePassword(BCrypUtil.encrypt(userPasswordRequest.newPassword()));
@@ -65,13 +63,13 @@ public class UserService {
 	/* 기타 메서드 */
 	public User findUserById(Long userId) {
 		return userRepository.findById(userId).orElseThrow(
-			() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "조회되는 회원 정보가 없습니다. id: " + userId)
+			() -> new NotFoundException(ErrorCode.USER_NOT_FOUND)
 		);
 	}
 
 	public User findUserByEmail(SigninRequest signinRequest) {
 		return userRepository.findByEmail(signinRequest.email()).orElseThrow(
-			() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "가입되지 않은 유저입니다.")
+			() -> new UnauthorizedException(ErrorCode.UNAUTHORIZED_USER)
 		);
 	}
 
