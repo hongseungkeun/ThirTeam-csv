7b5fc175ebe49c9ee2c47b8335ac8066e3b3da28
임PRO스터\su992 <su9926@naver.com>
2025-02-04 09:18:56 +0900

    Feat: 비밀번호 수정 기능 추가 및 @Transactional 적용

--- src/main/java/com/sparta/csv/domain/user/service/UserService.java
+++ src/main/java/com/sparta/csv/domain/user/service/UserService.java
@@ -1,10 +1,10 @@
 package com.sparta.csv.domain.user.service;
 
+import com.sparta.csv.global.config.BCrypUtil;
 import com.sparta.csv.global.exception.ForbiddenException;
 import com.sparta.csv.global.exception.error.ErrorCode;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
@@ -24,22 +24,42 @@ public class UserService {
 
 	private final UserRepository userRepository;
 
+	@Transactional(readOnly = true)
 	public UserInfoResponse getUser(Long userId) {
 		User user = findUserById(userId);
 
 		return UserInfoResponse.from(user);
 	}
 
+	@Transactional
 	public void deleteUser(Long userId) {
 		User user = findUserById(userId);
 
 		userRepository.delete(user);
 	}
 
+	@Transactional
 	public void updateUserById(Long userId, UserInfoRequest userInfoRequest) {
 		User user = findUserById(userId);
 
 		user.updateNickName(userInfoRequest.nickname());
+	}
+
+	@Transactional
+	public void updateUserPasswordById(Long userId, UserPasswordRequest userPasswordRequest) {
+		User user = userRepository.findById(userId).orElseThrow(
+			() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "조회되는 회원 정보가 없습니다. id: " + userId)
+		);
+
+		if (!BCrypUtil.matches(userPasswordRequest.password(), user.getPassword())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 비밀번호입니다.");
+		}
+
+		if (BCrypUtil.matches(userPasswordRequest.newPassword(), user.getPassword())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "기존과 동일한 비밀번호입니다.");
+		}
+
+		user.updatePassword(BCrypUtil.encrypt(userPasswordRequest.newPassword()));
 	}
 
 	/* 기타 메서드 */
@@ -62,22 +82,4 @@ public class UserService {
 			throw new ForbiddenException(ErrorCode.FORBIDDEN_USER_ACCESS);
 		}
 	}
-	@Transactional
-	public void updateUserPasswordById(Long userId, UserPasswordRequest userPasswordRequest) {
-		User user = userRepository.findById(userId).orElseThrow(
-			() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "조회되는 회원 정보가 없습니다. id: "+userId)
-		);
-
-		if(!BCrypUtil.matches(userPasswordRequest.password(), user.getPassword())){
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 비밀번호입니다.");
-		}
-
-		if(BCrypUtil.matches(userPasswordRequest.newPassword(), user.getPassword())){
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "기존과 동일한 비밀번호입니다.");
-		}
-
-		user.updatePassword(BCrypUtil.encrypt(userPasswordRequest.newPassword()));
-	}
-
-	/* Todo User 조회 메서드 따로 추출하기 */
 }
