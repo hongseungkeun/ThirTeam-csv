d3e621a519b94f032a72c2f7135b9de483d43d6e
SeuneKeun <eiziong1@gmail.com>
2025-02-06 18:43:20 +0900

    test: 테스트 케이스 변경
    - 실제 쓰레드 개수만큼 seatId를 랜덤하게 넣고 올바르게 작동하는지 확인하도록 수정

--- src/test/java/com/sparta/csv/domain/booking/service/BookingServiceTest.java
+++ src/test/java/com/sparta/csv/domain/booking/service/BookingServiceTest.java
@@ -16,6 +16,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -53,24 +54,39 @@ public class BookingServiceTest {
         // 예약을 테스트할 Screening, User 및 좌석 ID를 세팅함
         Long userId = 1L;
         Long screeningId = 1L;
-        List<Long> seatIds = List.of(1L, 2L); // 동일한 좌석 ID로 테스트
 
-        BookingCreateRequest request = new BookingCreateRequest(seatIds);
+        Random random = new Random();
+
+        int numberOfThreads = 500; // 동시 실행할 Thread 개수
+        List<BookingCreateRequest> request = new ArrayList<>();
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            List<Long> seatIds = new ArrayList<>();
+            int count = random.nextInt(5) + 1;
+
+            for (int j = 0; j < count; j++) {
+                long seat = random.nextLong(40) + 1L;
+                seatIds.add(seat);
+            }
+
+            BookingCreateRequest bc = new BookingCreateRequest(seatIds);
+            request.add(bc);
+        }
 
         // Thread 동시 실행을 위한 설정
-        int numberOfThreads = 10; // 동시 실행할 Thread 개수
         ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
         CyclicBarrier barrier = new CyclicBarrier(numberOfThreads);
 
         List<Future<Long>> results = new ArrayList<>();
 
         for (int i = 0; i < numberOfThreads; i++) {
+            int finalI = i;
             results.add(executorService.submit(() -> {
                 // 모든 Thread가 준비될 때까지 대기
                 barrier.await();
 
                 // registration 메소드 호출
-                return bookingService.createBooking(userId, screeningId, request);
+                return bookingService.createBooking(userId, screeningId, request.get(finalI));
             }));
         }
 
